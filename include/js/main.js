a156f2766d6377701c2a5d0a4192335a3f785339
l_spar02 <l_spar02@uni-muenster.de>
2024-03-19 22:37:34 +0100

    bug fixing

--- include/js/main.js
+++ include/js/main.js
@@ -7,7 +7,8 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer({
-	canvas: $("#bg"),
+	antialias: true,
+	canvas: document.querySelector('#bg'),
 });
 
 renderer.setPixelRatio( window.devicePixelRatio );
